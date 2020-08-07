9051915392b29bde28c8c3ba3f7cfccb65682f65
Kyome22 <kyomesuke@gmail.com>
2020-08-08 00:27:11 +0900

    add for windows div

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -10,7 +10,7 @@ export function Footer() {
       <div className="privacy-policy">
         <p className="contents-title">{t("privacy_policy")}</p>
         <button
-          className="privacy-link"
+          className="link"
           onClick={() => {
             window.location.href = t("privacy_policy_url");
           }}
@@ -36,6 +36,20 @@ export function Footer() {
           <img className="link-image" src="images/kyome_facebook.png" alt="" />
           <span>Takuto Nakamura</span>
         </a>
+      </div>
+      <div className="for-windows">
+        <p className="contents-title">RunCat for Windows</p>
+        <img src="images/runcat_for_windows_demo.gif" alt="demo" />
+        <p className="explain">{t("explain4")}</p>
+        <button
+          className="link"
+          onClick={() => {
+            window.location.href =
+              "https://github.com/Kyome22/RunCat_for_windows";
+          }}
+        >
+          {t("download")}
+        </button>
       </div>
       <p className="copyright">{t("copyright")}</p>
     </div>
